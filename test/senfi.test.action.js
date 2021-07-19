371587b508e3faaaa439fdc2a25cd633f6d60945
Jun Hao Yeo <junhao@gelement.com>
2021-07-19 11:58:31 +0800

    Modified to use before and after

--- test/senfi.test.action.js
+++ test/senfi.test.action.js
@@ -6,47 +6,43 @@ const config = { host: "api.dev.senfi.io" };
 const sinon = require("sinon");
 
 describe.only("Test senfi-node action.js", async function () {
+	this.beforeEach(function () {
+		sinon.stub(Senfi.prototype, "httpRequest").yields();
+	});
+
+	this.afterEach(function () {
+		Senfi.prototype.httpRequest.restore();
+	});
+
 	it("Email - Should call httpRequest", async function () {
-		let stub = sinon.stub(Senfi.prototype, "httpRequest").yields();
 		let senfi = Senfi();
 		await senfi.initialize(testData.key, testData.secret, config);
 		await senfi.action.email(null, null, null);
 
-		expect(stub.called).equal(true);
-
-		Senfi.prototype.httpRequest.restore();
+		expect(Senfi.prototype.httpRequest.called).equal(true);
 	});
 
 	it("SMS - Should call httpRequest", async function () {
-		let stub = sinon.stub(Senfi.prototype, "httpRequest").yields();
 		let senfi = Senfi();
 		await senfi.initialize(testData.key, testData.secret, config);
 		await senfi.action.sms(null, null, null);
 
-		expect(stub.called).equal(true);
-
-		Senfi.prototype.httpRequest.restore();
+		expect(Senfi.prototype.httpRequest.called).equal(true);
 	});
 
 	it("Telegram - Should call httpRequest", async function () {
-		let stub = sinon.stub(Senfi.prototype, "httpRequest").yields();
 		let senfi = Senfi();
 		await senfi.initialize(testData.key, testData.secret, config);
 		await senfi.action.telegram(null, null);
 
-		expect(stub.called).equal(true);
-
-		Senfi.prototype.httpRequest.restore();
+		expect(Senfi.prototype.httpRequest.called).equal(true);
 	});
 
 	it("Webhook - Should call httpRequest", async function () {
-		let stub = sinon.stub(Senfi.prototype, "httpRequest").yields();
 		let senfi = Senfi();
 		await senfi.initialize(testData.key, testData.secret, config);
 		await senfi.action.webhook(null, null, null, null);
 
-		expect(stub.called).equal(true);
-
-		Senfi.prototype.httpRequest.restore();
+		expect(Senfi.prototype.httpRequest.called).equal(true);
 	});
 });
