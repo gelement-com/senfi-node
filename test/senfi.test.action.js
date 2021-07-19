6adac0a6e00490b3660b8a370f7e6df556fea58d
Jun Hao Yeo <junhao@gelement.com>
2021-07-19 11:14:33 +0800

    Modify parameters

--- test/senfi.test.action.js
+++ test/senfi.test.action.js
@@ -6,22 +6,22 @@ const config = { host: "api.dev.senfi.io" };
 const sinon = require("sinon");
 
 describe.only("Test senfi-node action.js", async function () {
-	it("Email Should call httpRequest", async function () {
+	it("Email - Should call httpRequest", async function () {
 		let stub = sinon.stub(Senfi.prototype, "httpRequest").yields();
 		let senfi = Senfi();
 		await senfi.initialize(testData.key, testData.secret, config);
-		await senfi.action.email(["test@test.com"], "Senfi Title Test", "Senfi Content Test");
+		await senfi.action.email(null, null, null);
 
 		expect(stub.called).equal(true);
 
 		Senfi.prototype.httpRequest.restore();
 	});
 
-	it("SMS Should call httpRequest", async function () {
+	it("SMS - Should call httpRequest", async function () {
 		let stub = sinon.stub(Senfi.prototype, "httpRequest").yields();
 		let senfi = Senfi();
 		await senfi.initialize(testData.key, testData.secret, config);
-		await senfi.action.sms(["+6512345678"], "Senfi SMS Test");
+		await senfi.action.sms(null, null, null);
 
 		expect(stub.called).equal(true);
 
