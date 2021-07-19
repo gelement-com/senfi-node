9c6954d3a6de51cc55905c26ec2435f14e5a5eb3
Jun Hao Yeo <junhao@gelement.com>
2021-07-19 10:34:57 +0800

    Add test for sms

--- test/senfi.test.action.js
+++ test/senfi.test.action.js
@@ -6,16 +6,25 @@ const config = { host: "api.dev.senfi.io" };
 const sinon = require("sinon");
 
 describe.only("Test senfi-node action.js", async function () {
-	describe("Email Test", async function () {
-		it("Should call httpRequest", async function () {
-			let stub = sinon.stub(Senfi.prototype, "httpRequest").yields();
-			let senfi = Senfi();
-			await senfi.initialize(testData.key, testData.secret, config);
-			await senfi.action.email([testData.email], "Senfi Title Test", "Senfi Content Test");
+	it("Email Should call httpRequest", async function () {
+		let stub = sinon.stub(Senfi.prototype, "httpRequest").yields();
+		let senfi = Senfi();
+		await senfi.initialize(testData.key, testData.secret, config);
+		await senfi.action.email(["test@test.com"], "Senfi Title Test", "Senfi Content Test");
 
-			expect(stub.called).equal(true);
+		expect(stub.called).equal(true);
 
-			Senfi.prototype.httpRequest.restore();
-		});
+		Senfi.prototype.httpRequest.restore();
+	});
+
+	it("SMS Should call httpRequest", async function () {
+		let stub = sinon.stub(Senfi.prototype, "httpRequest").yields();
+		let senfi = Senfi();
+		await senfi.initialize(testData.key, testData.secret, config);
+		await senfi.action.sms(["+6512345678"], "Senfi SMS Test");
+
+		expect(stub.called).equal(true);
+
+		Senfi.prototype.httpRequest.restore();
 	});
 });
