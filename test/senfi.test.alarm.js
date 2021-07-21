6f46be28b15f741cad8764a987adf074b337a99a
Jun Hao Yeo <junhao@gelement.com>
2021-07-21 11:15:24 +0800

    Add test for alarm subscribe

---
+++ test/senfi.test.alarm.js
@@ -0,0 +1,26 @@
+const testData = require("./test-data.json");
+const Senfi = require("../lib/senfi");
+const chai = require("chai");
+const expect = chai.expect;
+const config = { host: "api.dev.senfi.io" };
+const sinon = require("sinon");
+
+describe.only("Test senfi-node alarm.js", async function () {
+	this.beforeEach(function () {
+		sinon.stub(Senfi.prototype, "httpRequest").yields();
+	});
+
+	this.afterEach(function () {
+		Senfi.prototype.httpRequest.restore();
+	});
+
+	it("Subscribe - Should receive success false due to invalid arguments on subscribing", async function () {
+		let senfi = Senfi();
+
+		await senfi.initialize(testData.key, testData.secret, config);
+		await senfi.alarm.subscribe().catch(function (err) {
+			expect(err).to.have.property("success");
+			expect(err.success).equal(false);
+		});
+	});
+});
