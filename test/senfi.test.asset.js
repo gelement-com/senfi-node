1b150017dd3f10866988dd0e5dd8a2b3666cd5ee
Jun Hao Yeo <junhao@gelement.com>
2021-07-22 14:33:39 +0800

    Add test for asset.js

---
+++ test/senfi.test.asset.js
@@ -0,0 +1,16 @@
+const testData = require("./test-data.json");
+const Senfi = require("../lib/senfi");
+const chai = require("chai");
+const expect = chai.expect;
+const config = { host: "api.dev.senfi.io" };
+const sinon = require("sinon");
+
+describe.only("Test senfi-node asset.js", async function () {
+	this.beforeEach(function () {
+		sinon.stub(Senfi.prototype, "httpRequest").yields();
+	});
+
+	this.afterEach(function () {
+		Senfi.prototype.httpRequest.restore();
+	});
+});
