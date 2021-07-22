4c59b4faaf2092b4cbf921a9b7db595823cc5d2e
Jun Hao Yeo <junhao@gelement.com>
2021-07-22 17:14:45 +0800

    Modify test for getAssetIdFromTag

--- test/senfi.test.asset.js
+++ test/senfi.test.asset.js
@@ -14,49 +14,54 @@ describe.only("Test senfi-node asset.js", async function () {
 		Senfi.prototype.httpRequest.restore();
 	});
 
-	it("Get - Should receive success false due to invalid arguments", async function () {
-		let senfi = Senfi();
+	describe("Get", async function () {
+		it("Should receive success false due to invalid arguments", async function () {
+			let senfi = Senfi();
 
-		await senfi.initialize(testData.key, testData.secret, config);
+			await senfi.initialize(testData.key, testData.secret, config);
 
-		try {
-			await senfi.asset.get();
-		} catch (err) {
-			expect(err).to.have.property("success");
-			expect(err.success).equal(false);
-		}
+			try {
+				await senfi.asset.get();
+			} catch (err) {
+				expect(err).to.have.property("success");
+				expect(err.success).equal(false);
+			}
+		});
+
+		it("should receive success false due to unexpected values", async function () {
+			let senfi = Senfi();
+
+			await senfi.initialize(testData.key, testData.secret, config);
+			try {
+				await senfi.asset.get({});
+			} catch (err) {
+				expect(err).to.have.property("success");
+				expect(err.success).equal(false);
+			}
+		});
+
+		it("should call httpRequest", async function () {
+			let senfi = Senfi();
+
+			await senfi.initialize(testData.key, testData.secret, config);
+			await senfi.asset.get({ site_id: null, asset_id: null, tag: null });
+
+			expect(Senfi.prototype.httpRequest.called).equal(true);
+		});
 	});
 
-	it("Get - should receive success false due to unexpected values", async function () {
-		let senfi = Senfi();
+	describe("getAssetIdFromTag", async function () {
+		it("getAssetIdFromTag - should call httpRequest", async function () {
+			let senfi = Senfi();
 
-		await senfi.initialize(testData.key, testData.secret, config);
-		try {
-			await senfi.asset.get({});
-		} catch (err) {
-			expect(err).to.have.property("success");
-			expect(err.success).equal(false);
-		}
-	});
+			await senfi.initialize(testData.key, testData.secret, config);
 
-	it("Get - should call httpRequest", async function () {
-		let senfi = Senfi();
-
-		await senfi.initialize(testData.key, testData.secret, config);
-		await senfi.asset.get({ site_id: null, asset_id: null, tag: null });
-
-		expect(Senfi.prototype.httpRequest.called).equal(true);
-	});
-
-	it("getAssetIdFromTag - should call httpRequest", async function () {
-		Senfi.prototype.httpRequest.restore();
-		sinon.stub(Senfi.prototype, "httpRequest").yields({ success: true, assets: [1] });
-
-		let senfi = Senfi();
-
-		await senfi.initialize(testData.key, testData.secret, config);
-		await senfi.asset.getAssetIdFromTag(null, {});
-		
-		expect(Senfi.prototype.httpRequest.called).equal(true);
+			try {
+				await senfi.asset.getAssetIdFromTag();
+			} catch (ex) {
+				// testing if httpRequest is called. Not testing if success is true
+				expect(Senfi.prototype.httpRequest.called).equal(true);
+			}
+		});
 	});
 });
