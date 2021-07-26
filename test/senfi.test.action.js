70220a6e9820d452d06ad6ff7a94be4a61b84f12
Jun Hao Yeo <junhao@gelement.com>
2021-07-26 11:24:05 +0800

    Add test for error throw

--- test/senfi.test.action.js
+++ test/senfi.test.action.js
@@ -14,12 +14,29 @@ describe.only("Test senfi-node action.js", async function () {
 		Senfi.prototype.httpRequest.restore();
 	});
 
-	it("Email - Should call httpRequest", async function () {
-		let senfi = Senfi();
-		await senfi.initialize(testData.key, testData.secret, config);
-		await senfi.action.email(null, null, null);
+	describe("Email", async function () {
+		it("Should call httpRequest", async function () {
+			let senfi = Senfi();
+			await senfi.initialize(testData.key, testData.secret, config);
+			await senfi.action.email(null, null, null);
 
-		expect(Senfi.prototype.httpRequest.called).equal(true);
+			expect(Senfi.prototype.httpRequest.called).equal(true);
+		});
+
+		it("Should return errcode exception", async function () {
+			Senfi.prototype.httpRequest.restore();
+			sinon.stub(Senfi.prototype, "httpRequest").throws();
+
+			let senfi = Senfi();
+			await senfi.initialize(testData.key, testData.secret, config);
+
+			try {
+				await senfi.action.email(null, null, null);
+			} catch (err) {
+				expect(err).to.have.property("errcode");
+				expect(err.errcode).equal("exception");
+			}
+		});
 	});
 
 	it("SMS - Should call httpRequest", async function () {
