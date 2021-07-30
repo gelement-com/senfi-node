2d2aa6f27d7605dee2c832fb00b0d20bcd10b16a
Jun Hao Yeo <junhao@gelement.com>
2021-07-30 11:10:21 +0800

    Add all test for generate

--- test/senfi.test.event.js
+++ test/senfi.test.event.js
@@ -84,4 +84,250 @@ describe.only("Test senfi-node event.js", async function () {
 			await expect(senfi.event.unsubscribe("")).to.be.rejected;
 		});
 	});
+
+	describe("Generate", async function () {
+		it("Should be rejected if argument eventDefId is not number and embeddedInputs is not object and is not null", async function () {
+			let senfi = Senfi();
+
+			await senfi.initialize(testData.key, testData.secret, config);
+
+			// eventDefId, type, embeddedInputs, eventParams
+			await expect(senfi.event.generate("", "", "", {})).to.be.rejected;
+		});
+
+		it("Should be rejected if argument embeddedInputs is not object", async function () {
+			let senfi = Senfi();
+
+			await senfi.initialize(testData.key, testData.secret, config);
+
+			// eventDefId, type, embeddedInputs, eventParams
+			await expect(senfi.event.generate(0, "measurement", "", {})).to.be.rejected;
+		});
+
+		it("Should be rejected if argument eventParams is not object", async function () {
+			let senfi = Senfi();
+
+			await senfi.initialize(testData.key, testData.secret, config);
+
+			// eventDefId, type, embeddedInputs, eventParams
+			await expect(senfi.event.generate(0, "measurement", {}, "")).to.be.rejected;
+		});
+
+		it("Should be rejected if eventDefId is not a number", async function () {
+			let senfi = Senfi();
+
+			await senfi.initialize(testData.key, testData.secret, config);
+
+			// eventDefId, type, embeddedInputs, eventParams
+			await expect(senfi.event.generate("", "measurement", {}, { metric_caused: [], measurement_snapshot: [] })).to.be.rejected;
+		});
+
+		it("Should be rejected if argument type is not listed", async function () {
+			let senfi = Senfi();
+
+			await senfi.initialize(testData.key, testData.secret, config);
+
+			// eventDefId, type, embeddedInputs, eventParams
+			await expect(senfi.event.generate(0, "", {}, {})).to.be.rejected;
+		});
+
+		describe("When argument type is 'measurement'", async function () {
+			it("Should be rejected if eventParams is not object", async function () {
+				let senfi = Senfi();
+
+				await senfi.initialize(testData.key, testData.secret, config);
+
+				// eventDefId, type, embeddedInputs, eventParams
+				await expect(senfi.event.generate(0, "measurement", {}, 0)).to.be.rejected;
+			});
+
+			it("Should be rejected if eventParams is null", async function () {
+				let senfi = Senfi();
+
+				await senfi.initialize(testData.key, testData.secret, config);
+
+				// eventDefId, type, embeddedInputs, eventParams
+				await expect(senfi.event.generate(0, "measurement", {}, null)).to.be.rejected;
+			});
+
+			it("Should be rejected if eventParams contain unexpected properties", async function () {
+				let senfi = Senfi();
+
+				await senfi.initialize(testData.key, testData.secret, config);
+
+				// eventDefId, type, embeddedInputs, eventParams
+				await expect(senfi.event.generate(0, "measurement", {}, { testproperty: 1 })).to.be.rejected;
+			});
+
+			it("Should be rejected if eventParams metric_caused is not array", async function () {
+				let senfi = Senfi();
+
+				await senfi.initialize(testData.key, testData.secret, config);
+
+				// eventDefId, type, embeddedInputs, eventParams
+				await expect(senfi.event.generate(0, "measurement", {}, { metric_caused: 1 })).to.be.rejected;
+			});
+
+			it("Should be rejected if eventParams measurement_snapshot is not array", async function () {
+				let senfi = Senfi();
+
+				await senfi.initialize(testData.key, testData.secret, config);
+
+				// eventDefId, type, embeddedInputs, eventParams
+				await expect(senfi.event.generate(0, "measurement", {}, { measurement_snapshot: 1 })).to.be.rejected;
+			});
+		});
+
+		describe("When argument type is 'event'", async function () {
+			it("Should be rejected if eventParams is not object", async function () {
+				let senfi = Senfi();
+
+				await senfi.initialize(testData.key, testData.secret, config);
+
+				// eventDefId, type, embeddedInputs, eventParams
+				await expect(senfi.event.generate(0, "event", {}, 0)).to.be.rejected;
+			});
+
+			it("Should be rejected if eventParams is null", async function () {
+				let senfi = Senfi();
+
+				await senfi.initialize(testData.key, testData.secret, config);
+
+				// eventDefId, type, embeddedInputs, eventParams
+				await expect(senfi.event.generate(0, "event", {}, null)).to.be.rejected;
+			});
+
+			it("Should be rejected if eventParams contain unexpected properties", async function () {
+				let senfi = Senfi();
+
+				await senfi.initialize(testData.key, testData.secret, config);
+
+				// eventDefId, type, embeddedInputs, eventParams
+				await expect(senfi.event.generate(0, "event", {}, { testproperty: 1 })).to.be.rejected;
+			});
+
+			it("Should be rejected if eventParams event_data is not array", async function () {
+				let senfi = Senfi();
+
+				await senfi.initialize(testData.key, testData.secret, config);
+
+				// eventDefId, type, embeddedInputs, eventParams
+				await expect(senfi.event.generate(0, "event", {}, { event_data: 1 })).to.be.rejected;
+			});
+		});
+
+		describe("When argument type is 'alarm'", async function () {
+			it("Should be rejected if eventParams is not object", async function () {
+				let senfi = Senfi();
+
+				await senfi.initialize(testData.key, testData.secret, config);
+
+				// eventDefId, type, embeddedInputs, eventParams
+				await expect(senfi.event.generate(0, "alarm", {}, 0)).to.be.rejected;
+			});
+
+			it("Should be rejected if eventParams is null", async function () {
+				let senfi = Senfi();
+
+				await senfi.initialize(testData.key, testData.secret, config);
+
+				// eventDefId, type, embeddedInputs, eventParams
+				await expect(senfi.event.generate(0, "alarm", {}, null)).to.be.rejected;
+			});
+
+			it("Should be rejected if eventParams contain unexpected properties", async function () {
+				let senfi = Senfi();
+
+				await senfi.initialize(testData.key, testData.secret, config);
+
+				// eventDefId, type, embeddedInputs, eventParams
+				await expect(senfi.event.generate(0, "alarm", {}, { testproperty: 1 })).to.be.rejected;
+			});
+
+			it("Should be rejected if eventParams alarm_data is not array", async function () {
+				let senfi = Senfi();
+
+				await senfi.initialize(testData.key, testData.secret, config);
+
+				// eventDefId, type, embeddedInputs, eventParams
+				await expect(senfi.event.generate(0, "alarm", {}, { alarm_data: 1 })).to.be.rejected;
+			});
+		});
+
+		describe("When argument type is 'command'", async function () {
+			it("Should be rejected if eventParams is not object", async function () {
+				let senfi = Senfi();
+
+				await senfi.initialize(testData.key, testData.secret, config);
+
+				// eventDefId, type, embeddedInputs, eventParams
+				await expect(senfi.event.generate(0, "command", {}, 0)).to.be.rejected;
+			});
+
+			it("Should be rejected if eventParams is null", async function () {
+				let senfi = Senfi();
+
+				await senfi.initialize(testData.key, testData.secret, config);
+
+				// eventDefId, type, embeddedInputs, eventParams
+				await expect(senfi.event.generate(0, "command", {}, null)).to.be.rejected;
+			});
+
+			it("Should be rejected if eventParams contain unexpected properties", async function () {
+				let senfi = Senfi();
+
+				await senfi.initialize(testData.key, testData.secret, config);
+
+				// eventDefId, type, embeddedInputs, eventParams
+				await expect(senfi.event.generate(0, "command", {}, { testproperty: 1 })).to.be.rejected;
+			});
+
+			it("Should be rejected if eventParams command_data is not array", async function () {
+				let senfi = Senfi();
+
+				await senfi.initialize(testData.key, testData.secret, config);
+
+				// eventDefId, type, embeddedInputs, eventParams
+				await expect(senfi.event.generate(0, "command", {}, { command_data: 1 })).to.be.rejected;
+			});
+		});
+
+		describe("When argument type is 'log'", async function () {
+			it("Should be rejected if eventParams is not object", async function () {
+				let senfi = Senfi();
+
+				await senfi.initialize(testData.key, testData.secret, config);
+
+				// eventDefId, type, embeddedInputs, eventParams
+				await expect(senfi.event.generate(0, "log", {}, 0)).to.be.rejected;
+			});
+
+			it("Should be rejected if eventParams is null", async function () {
+				let senfi = Senfi();
+
+				await senfi.initialize(testData.key, testData.secret, config);
+
+				// eventDefId, type, embeddedInputs, eventParams
+				await expect(senfi.event.generate(0, "log", {}, null)).to.be.rejected;
+			});
+
+			it("Should be rejected if eventParams contain unexpected properties", async function () {
+				let senfi = Senfi();
+
+				await senfi.initialize(testData.key, testData.secret, config);
+
+				// eventDefId, type, embeddedInputs, eventParams
+				await expect(senfi.event.generate(0, "log", {}, { testproperty: 1 })).to.be.rejected;
+			});
+
+			it("Should be rejected if eventParams log_data is not array", async function () {
+				let senfi = Senfi();
+
+				await senfi.initialize(testData.key, testData.secret, config);
+
+				// eventDefId, type, embeddedInputs, eventParams
+				await expect(senfi.event.generate(0, "log", {}, { log_data: 1 })).to.be.rejected;
+			});
+		});
+	});
 });
