eebefb8b2273f63dea662e7cda3df83f990aa1b9
Jun Hao Yeo <junhao@gelement.com>
2021-07-30 12:09:41 +0800

    Add test for subscribe

--- test/senfi.test.log.js
+++ test/senfi.test.log.js
@@ -9,11 +9,45 @@ const chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
 
 describe.only("Test senfi-node log.js", async function () {
-    this.beforeEach(function () {
+	this.beforeEach(function () {
 		sinon.stub(Senfi.prototype, "httpRequest").yields();
 	});
 
 	this.afterEach(function () {
 		Senfi.prototype.httpRequest.restore();
+	});
+
+	describe.only("Subscribe", async function () {
+		it("Should be rejected when argument is not object", async function () {
+			let senfi = Senfi();
+
+			await senfi.initialize(testData.key, testData.secret, config);
+			await expect(senfi.log.subscribe("")).to.be.rejected;
+		});
+
+		it("Should be rejected when argument does not contain unexpected property", async function () {
+			let senfi = Senfi();
+
+			await senfi.initialize(testData.key, testData.secret, config);
+			await expect(senfi.log.subscribe({ testproperty: 1 })).to.be.rejected;
+		});
+
+		it("should call httpRequest", async function () {
+			let senfi = Senfi();
+
+			await senfi.initialize(testData.key, testData.secret, config);
+			await senfi.log.subscribe({ component: "" });
+
+			expect(Senfi.prototype.httpRequest.called).equal(true);
+		});
+
+		it("should be rejected by httpRequest when throw error", async function () {
+			Senfi.prototype.httpRequest.restore();
+			sinon.stub(Senfi.prototype, "httpRequest").throws();
+			let senfi = Senfi();
+
+			await senfi.initialize(testData.key, testData.secret, config);
+			await expect(senfi.log.subscribe({ component: [] })).to.be.rejected;
+		});
 	});
 });
