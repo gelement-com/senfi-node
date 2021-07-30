16aff6a50d50ead6d9b1b5c4180b6d3cd3c9de35
Jun Hao Yeo <junhao@gelement.com>
2021-07-30 10:01:02 +0800

    init test event

---
+++ test/senfi.test.event.js
@@ -0,0 +1,19 @@
+const testData = require("./test-data.json");
+const Senfi = require("../lib/senfi");
+const chai = require("chai");
+const expect = chai.expect;
+const config = { host: "api.dev.senfi.io" };
+const sinon = require("sinon");
+const chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+
+describe.only("Test senfi-node event.js", async function () {
+	this.beforeEach(function () {
+		sinon.stub(Senfi.prototype, "httpRequest").yields();
+	});
+
+	this.afterEach(function () {
+		Senfi.prototype.httpRequest.restore();
+	});
+});
