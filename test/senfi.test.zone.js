c95853da00fabe6fdf5d695763a764abbf5d61a4
Jun Hao Yeo <junhao@gelement.com>
2022-03-11 10:52:16 +0800

    Add test for isAssetInZone

--- test/senfi.test.zone.js
+++ test/senfi.test.zone.js
@@ -15,4 +15,37 @@ describe("Test senfi-node zone.js", async function () {
 	this.afterEach(function () {
 		Senfi.prototype.httpRequest.restore();
 	});
+
+	describe("isAssetInZone", async function () {
+		it("Should be rejected if thrown an error", async function () {
+			let senfi = Senfi();
+
+			Senfi.prototype.httpRequest.restore();
+			sinon.stub(Senfi.prototype, "httpRequest").throws(new Error("This is a test"));
+
+			await senfi.initialize(testData.key, testData.secret, { host: "api.dev.senfi.io" });
+			await expect(senfi.zone.isAssetInZone(0, 0)).eventually.be.rejected.and.include({
+				success: false,
+				errmsg: "This is a test",
+				errcode: "sdk_exception"
+			});
+		});
+
+		it("Should be resolved", async function () {
+			let senfi = Senfi();
+
+			Senfi.prototype.httpRequest.restore();
+			sinon.stub(Senfi.prototype, "httpRequest").yields({
+				success: true,
+				zone_result: true
+			});
+
+			await senfi.initialize(testData.key, testData.secret, { host: "api.dev.senfi.io" });
+
+			await expect(senfi.zone.isAssetInZone(0, 0)).eventually.be.fulfilled.and.include({
+				success: true,
+				zone_result: true
+			});
+		});
+	});
 });
