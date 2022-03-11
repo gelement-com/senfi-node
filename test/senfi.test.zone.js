5915340219b48827719f4a120696bf1a57dc0ac4
Jun Hao Yeo <junhao@gelement.com>
2022-03-11 10:57:33 +0800

    Add test for isPointInZone

--- test/senfi.test.zone.js
+++ test/senfi.test.zone.js
@@ -112,4 +112,36 @@ describe("Test senfi-node zone.js", async function () {
 			});
 		});
 	});
+
+	describe("isPointInZone", async function () {
+		it("Should be rejected if thrown an error", async function () {
+			let senfi = Senfi();
+
+			Senfi.prototype.httpRequest.restore();
+			sinon.stub(Senfi.prototype, "httpRequest").throws(new Error("This is a test"));
+
+			await senfi.initialize(testData.key, testData.secret, { host: "api.dev.senfi.io" });
+			await expect(senfi.zone.isPointInZone(0, 0, 0, 0)).eventually.be.rejected.and.include({
+				success: false,
+				errmsg: "This is a test",
+				errcode: "sdk_exception"
+			});
+		});
+
+		it("Should be resolved", async function () {
+			let senfi = Senfi();
+
+			Senfi.prototype.httpRequest.restore();
+			sinon.stub(Senfi.prototype, "httpRequest").yields({
+				success: true,
+				zone_result: true
+			});
+
+			await senfi.initialize(testData.key, testData.secret, { host: "api.dev.senfi.io" });
+			await expect(senfi.zone.isPointInZone(0, 0, 0, 0)).eventually.be.fulfilled.and.include({
+				success: true,
+				zone_result: true
+			});
+		});
+	});
 });
