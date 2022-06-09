dba50917149ae929965f75205b9cfb6e63d9eac2
Cheng Teng <chengteng@gelement.com>
2022-06-09 17:54:41 +0800

    Initialization returns object with success, errcode, errmsg Resource requests also return more specific error code on failure. e.g. 'unauthorized' Add jsdoc for return types Update tests

--- test/senfi.test.js
+++ test/senfi.test.js
@@ -18,23 +18,25 @@ describe('senfi.initialize()', function() {
     var senfi = Senfi();
     return senfi.initialize('Simulate01', '19cb6de1-9ab9-4a65-9c83-a3e507686a27')
     .then((res) => {
-      assert.strictEqual(res, true);
+      assert.strictEqual(res.success, true);
     });
   });
 
   it('no credentials', function() {
     var senfi = Senfi();
     return senfi.initialize()
-    .catch((error) => {
-      assert.strictEqual(error, 'Initialization failed: Unauthorized. Invalid credentials.');
+    .catch((res) => {
+      assert.strictEqual(res.success, false);
+      assert.strictEqual(res.errcode, 'unauthorized');
     })
   });
 
   it('invalid credentials', function() {
     var senfi = Senfi();
     return senfi.initialize('foo', 'bar')
-    .catch((error) => {
-      assert.strictEqual(error, 'Initialization failed: Unauthorized. Invalid credentials.');
+    .catch((res) => {
+      assert.strictEqual(res.success, false);
+      assert.strictEqual(res.errcode, 'unauthorized');
     })
   });
 
@@ -42,7 +44,7 @@ describe('senfi.initialize()', function() {
     var senfi = Senfi();
     return senfi.initialize('Simulate01', '19cb6de1-9ab9-4a65-9c83-a3e507686a27', {})
     .then((res) => {
-      assert.strictEqual(res, true);
+      assert.strictEqual(res.success, true);
     });
   });
 });
